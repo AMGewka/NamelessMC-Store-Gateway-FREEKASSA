0a7bf4d084a8051006c00b318fdf41f50939265c
AMGewka <devid.gabibov@gmail.com>
2024-11-21 20:56:15 +0500

    Update 1.8.5
    Обновление кода:
    - Добавлен украинский язык
    - Добавлено автозаполнение email покупателя
    - Добавлена строка поддерживаемых валют
    - Добавлена автогенерация ссылок "URL Оповещения" и "URL успешной оплаты"

--- Store_1.8/modules/Store/gateways/FREEKASSA/gateway.php
+++ Store_1.8/modules/Store/gateways/FREEKASSA/gateway.php
@@ -4,100 +4,106 @@
  *
  * @package Modules\Store
  * @author AMGewka
- * @version 1.8.4
+ * @version 1.8.5
  * @license MIT
  */
-class FREEKASSA_Gateway extends GatewayBase {
+class FREEKASSA_Gateway extends GatewayBase
+{
+	public function __construct()
+	{
+		$name = 'FREEKASSA';
+		$author = '<a href="https://github.com/AMGewka" target="_blank" rel="nofollow noopener">AMGewka</a>';
+		$gateway_version = '1.8.5';
+		$store_version = '1.7.1';
+		$settings = ROOT_PATH . '/modules/Store/gateways/FREEKASSA/gateway_settings/settings.php';
 
-    public function __construct() {
-        $name = 'FREEKASSA';
-        $author = '<a href="https://github.com/AMGewka" target="_blank" rel="nofollow noopener">AMGewka</a>';
-        $gateway_version = '1.8.4';
-        $store_version = '1.7.1';
-        $settings = ROOT_PATH . '/modules/Store/gateways/FREEKASSA/gateway_settings/settings.php';
+		parent::__construct($name, $author, $gateway_version, $store_version, $settings);
+	}
 
-        parent::__construct($name, $author, $gateway_version, $store_version, $settings);
-    }
+	public function onCheckoutPageLoad(TemplateBase $template, Customer $customer) : void {}
 
-    public function onCheckoutPageLoad(TemplateBase $template, Customer $customer): void {}
+	public function processOrder(Order $order) : void
+	{
+		$shopId = StoreConfig::get('FREEKASSA.shopid_key');
+		$apiKey = StoreConfig::get('FREEKASSA.shopapi_key');
+		$secretWord = StoreConfig::get('FREEKASSA.secret1_key');
 
-    public function processOrder(Order $order): void {
-        $shopId = StoreConfig::get('FREEKASSA.shopid_key');
-        $apiKey = StoreConfig::get('FREEKASSA.shopapi_key');
-        $secretWord = StoreConfig::get('FREEKASSA.secret1_key');
-        
-        if ($shopId == null || empty($shopId)) {
-            $this->addError('The administration has not completed the configuration of this gateway!');
-            return;
-        }
+		if ($shopId == null || empty($shopId)) {
+			$this->addError('The administration has not completed the configuration of this gateway!');
+			return;
+		}
 
-        $paymentId = $order->data()->id;
-        $orderAmount = $order->getAmount()->getTotalCents() / 100;
-        $currency = $order->getAmount()->getCurrency();
+		$paymentId = $order->data()->id;
+		$orderAmount = $order->getAmount()->getTotalCents() / 100;
+		$currency = $order->getAmount()->getCurrency();
+		$email = $order->customer()->getUser()->data()->email;
+		if (empty($email)) {
+			$email = 'pleaseinput@email.com';
+		}
+		$sign = md5($shopId . ':' . $orderAmount . ':' . $secretWord . ':' . $currency . ':' . $paymentId);
 
-        $sign = md5($shopId . ':' . $orderAmount . ':' . $secretWord . ':' . $currency . ':' . $paymentId);
+		$paymentUrl = 'https://pay.freekassa.ru/?m=' . $shopId . '&oa=' . $orderAmount . '&o=' . $paymentId . '&s=' . $sign . '&currency=' . $currency . '&em=' . $email;
 
-        $paymentUrl = 'https://pay.freekassa.ru/?m=' . $shopId . '&oa=' . $orderAmount . '&o=' . $paymentId . '&s=' . $sign . '&currency=' . $currency;
+		header('Location: ' . $paymentUrl);
+	}
 
-        header('Location: ' . $paymentUrl);
-    }
+	public function handleReturn() : bool
+	{
+		if (isset($_GET['do']) && $_GET['do'] == 'success') {
+			header("Location: " . URL::getSelfURL(), '/') . URL::build('/store/');
+		}
 
-    public function handleReturn(): bool {
-        if (isset($_GET['do']) && $_GET['do'] == 'success') {
-            header("Location: " . URL::getSelfURL(), '/') . URL::build('/store/');
-        }
+		return false;
+	}
 
-        return false;
-    }
+	public function handleListener() : void
+	{
+		$shopId = StoreConfig::get('FREEKASSA.shopid_key');
+		$apiKey = StoreConfig::get('FREEKASSA.shopapi_key');
+		$secretWord = StoreConfig::get('FREEKASSA.secret2_key');
 
-    public function handleListener(): void {
-        $shopId = StoreConfig::get('FREEKASSA.shopid_key');
-        $apiKey = StoreConfig::get('FREEKASSA.shopapi_key');
-        $secretWord = StoreConfig::get('FREEKASSA.secret2_key');
+		$allowedIps = array(
+			'168.119.157.136',
+			'168.119.60.227',
+			'51.250.54.238',
+			'178.154.197.79',
+		);
 
-        $allowedIps = array(
-            '168.119.157.136', 
-            '168.119.60.227', 
-            '51.250.54.238',
-            '178.154.197.79',
-        );
+		if (!in_array($_SERVER['REMOTE_ADDR'], $allowedIps)) {
+			die("bad ip!");
+		}
 
-        if(!in_array($_SERVER['REMOTE_ADDR'], $allowedIps)){
-            die("bad ip!");
-        }
+		$sign = md5($_REQUEST['MERCHANT_ID'] . ':' . $_REQUEST['AMOUNT'] . $secretWord . $_REQUEST['MERCHANT_ORDER_ID']);
 
-        $sign = md5($_REQUEST['MERCHANT_ID'] . ':' . $_REQUEST['AMOUNT'] . $secretWord . $_REQUEST['MERCHANT_ORDER_ID']);
+		if ($sign != $_REQUEST['SIGN']) {
+			die("Signature error!");
+		}
 
-        if($sign != $_REQUEST['SIGN']){
-            die("Signature error!");
-        }
+		$paymentId = $_REQUEST['MERCHANT_ORDER_ID'];
+		$orderAmount = $_REQUEST['AMOUNT'];
+		$currency = $data['currency'];
 
-        $paymentId = $_REQUEST['MERCHANT_ORDER_ID'];
-        $orderAmount = $_REQUEST['AMOUNT'];
-        $currency = $data['currency'];
+		$payment = new Payment($paymentId, 'transaction');
+		if ($payment->exists()) {
+			$data = [
+				'transaction' => $paymentId,
+				'amount_cents' => Store::toCents($orderAmount),
+				'currency' => $currency,
+				'fee_cents' => '0'
+			];
+		} else {
+			$data = [
+				'order_id' => $paymentId,
+				'gateway_id' => $this->getId(),
+				'transaction' => $paymentId,
+				'amount_cents' => Store::toCents($orderAmount),
+				'currency' => $currency,
+				'fee_cents' => '0'
+			];
+		}
 
-        $payment = new Payment($paymentId, 'transaction');
-        if ($payment->exists()) {
-            $data = [
-                'transaction' => $paymentId,
-                'amount_cents' => Store::toCents($orderAmount),
-                'currency' => $currency,
-                'fee_cents' => '0'
-            ];
-        } else {
-            $data = [
-                'order_id' => $paymentId,
-                'gateway_id' => $this->getId(),
-                'transaction' => $paymentId,
-                'amount_cents' => Store::toCents($orderAmount),
-                'currency' => $currency,
-                'fee_cents' => '0'
-            ];
-        }
-
-        $payment->handlePaymentEvent(Payment::COMPLETED, $data);
-    }
-
+		$payment->handlePaymentEvent(Payment::COMPLETED, $data);
+	}
 }
 
 $gateway = new FREEKASSA_Gateway();
